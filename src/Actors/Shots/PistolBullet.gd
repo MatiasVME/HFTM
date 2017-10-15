126aa35d3f184cd857e3fd0d2c14dc6516d372ee
Matías Muñoz Espinoza <writkas2@gmail.com>
2017-10-15 14:40:40 -0300

    solucionado: ahora dispara respecto a la pistola

--- src/Actors/Shots/PistolBullet.gd
+++ src/Actors/Shots/PistolBullet.gd
@@ -5,7 +5,7 @@ func fire(this, mouse_pos, delta):
 		this.get_parent().add_child(self)
 		self.add_to_group("Bullets")
 		#self.set_position(this.get_position())
-		self.set_position(this.get_node("Image/Gun").get_position())
+		self.set_position(this.get_node("Image/Gun").get_global_position())
 		self.apply_impulse(Vector2(0,0), mouse_pos * delta * 200)
 		ShotManager.fire(5)
 		print(this.get_node("Image/Gun").get_position())
