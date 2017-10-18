d9a423c547aeed771487c86914592ff9ff48c1a8
Matías Muñoz Espinoza <writkas2@gmail.com>
2017-10-17 23:23:14 -0300

    Trabajando en el HUD del juego

--- Game/Actors/Shots/PistolBullet.gd
+++ Game/Actors/Shots/PistolBullet.gd
@@ -4,11 +4,9 @@ func fire(this, mouse_pos, delta):
 	if ShotManager.can_fire:
 		this.get_parent().add_child(self)
 		self.add_to_group("Bullets")
-		#self.set_position(this.get_position())
 		self.set_position(this.get_node("Image/Gun").get_global_position())
 		self.apply_impulse(Vector2(0,0), mouse_pos * delta * 200)
-		ShotManager.fire(5)
-		print(this.get_node("Image/Gun").get_position())
+		ShotManager.fire(20)
 
 func _on_Destroy_timeout():
 	self.queue_free()
