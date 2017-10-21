3aece945b503741c28b6ec7b13af4f146540a807
Matías Muñoz Espinoza <writkas2@gmail.com>
2017-10-21 17:15:37 -0300

    Trabajando en en que el player pueda entrar al auto

--- Game/Actors/Players/Player.gd
+++ Game/Actors/Players/Player.gd
@@ -2,11 +2,15 @@ extends KinematicBody2D
 
 onready var camera = $Camera
 onready var img = $Image
+onready var touch_area = $TouchArea
 
 var pistol_bullet = preload("res://Game/Actors/Shots/PistolBullet.tscn")
 
 const SPEED = 100
 const MOVEMENT = 2
+
+var is_in_area = false
+var current_area = null
 
 func _ready():
 	set_physics_process(true)
@@ -30,6 +34,13 @@ func _physics_process(delta):
 		camera.zoom.x += 0.1
 		camera.zoom.y += 0.1
 	
+	if Input.is_action_just_pressed("use") and is_in_area:
+		if current_area is Area2D and current_area.get_groups().has("EnterCarPlayerArea"):
+			var car = current_area.get_parent()
+			car.can_move = true
+			car.get_node("Camera").current = true
+			self.queue_free()
+	
 	var mouse_global_pos = camera.get_global_mouse_position()
 	img.look_at(mouse_global_pos)
 	
@@ -38,3 +49,11 @@ func _physics_process(delta):
 	if Input.is_action_pressed("right_click"):
 		var i_pistol_bullet = pistol_bullet.instance()
 		i_pistol_bullet.fire(self, mouse_local_pos, delta)
+
+
+func _on_TouchArea_area_entered( area ):
+	is_in_area = true
+	current_area = area
+	
+func _on_TouchArea_area_exited( area ):
+	is_in_area = false
