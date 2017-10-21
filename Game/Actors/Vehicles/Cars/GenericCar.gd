f1db6202ce22bc01a63775b7e71d6fb8341775a9
Matías Muñoz Espinoza <writkas2@gmail.com>
2017-10-21 00:29:50 -0300

    Refactorizando el auto

--- Game/Actors/Vehicles/Cars/GenericCar.gd
+++ Game/Actors/Vehicles/Cars/GenericCar.gd
@@ -1,22 +1,63 @@
 extends KinematicBody2D
 
+export (float) var mass = 3
+export (float) var max_speed = 600
+export (bool) var can_move = false
+
+var motion = Vector2()
+var target_motion = Vector2()
+var steering = Vector2()
+
 var direction = Vector2()
 var speed = 0
 var acceleration = 1800
 var decceleration = 1000
 
-var world
-var result_up 
-var result_down
-var result_left
-var result_right
+var Skin = null
+var target_angle = 0
 
 func _ready():
 	set_physics_process(true)
-	world = get_world_2d().get_direct_space_state()
 
 func _physics_process(delta):
-	result_up = world.intersect_point(get_position() + Vector2(0, -64))
-	result_down = world.intersect_point(get_position() + Vector2(0, 64))
-	result_left = world.intersect_point(get_position() + Vector2(-64, 0))
-	result_right = world.intersect_point(get_position() + Vector2(64, 0))
+	if can_move:
+		input_car()
+		move_car(delta)
+
+func input_car():	
+	direction = Vector2()
+
+	if Input.is_action_pressed("up"):
+		direction.y = -1
+	elif Input.is_action_pressed("down"):
+		direction.y = 1
+
+	if Input.is_action_pressed("left"):
+		direction.x = -1
+	elif Input.is_action_pressed("right"):
+		direction.x = 1
+	
+func move_car(delta):
+	if direction != Vector2():
+		speed += acceleration * delta
+	else:
+		speed -= decceleration * delta
+
+	speed = clamp(speed, 0, max_speed)
+	
+	target_motion = speed * direction.normalized() * delta
+	steering = target_motion - motion
+
+	if steering.length() > 1:
+		steering = steering.normalized()
+	
+	motion += steering / mass
+
+	if speed == 0:
+		motion = Vector2()
+
+	move_and_collide(motion)
+
+	if motion != Vector2():
+		target_angle = atan2(motion.x, motion.y) - PI / 2
+		Skin.rotation = -target_angle
--- Game/Actors/Vehicles/Cars/NJC-104/NJC-104.gd
+++ Game/Actors/Vehicles/Cars/NJC-104/NJC-104.gd
@@ -1,55 +1,7 @@
 extends "../GenericCar.gd"
 #extends KinematicBody2D
 
-var motion = Vector2()
-var target_motion = Vector2()
-var steering = Vector2()
-
-const MASS = 2
-const MAX_SPEED = 600
-
-var Skin = null
-var target_angle = 0
-
 func _ready():
 	Skin = $Image
-	set_physics_process(true)
-
-func _physics_process(delta):
-	direction = Vector2()
-	
-	# por el momento
-
-	if Input.is_action_pressed("up"):
-		direction.y = -1
-	elif Input.is_action_pressed("down"):
-		direction.y = 1
-
-	if Input.is_action_pressed("left"):
-		direction.x = -1
-	elif Input.is_action_pressed("right"):
-		direction.x = 1
-	
-	if direction != Vector2():
-		speed += acceleration * delta
-	else:
-		speed -= decceleration * delta
-
-	speed = clamp(speed, 0, MAX_SPEED)
-	
-	target_motion = speed * direction.normalized() * delta
-	steering = target_motion - motion
-
-	if steering.length() > 1:
-		steering = steering.normalized()
-	
-	motion += steering / MASS
-
-	if speed == 0:
-		motion = Vector2()
-
-	move_and_collide(motion)
-
-	if motion != Vector2():
-		target_angle = atan2(motion.x, motion.y) - PI / 2
-		Skin.rotation = -target_angle
+	mass = 3
+	max_speed = 700
