52d434218fa7c689c8b985fce799980badab0c48
Matías Muñoz Espinoza <writkas2@gmail.com>
2017-10-22 22:37:26 -0300

    Traducciones

--- Game/MainScreens/MainMenu.gd
+++ Game/MainScreens/MainMenu.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 func _ready():
-	pass
+	get_node("Modes/Label").text = tr("MENU_MODES")
+	get_node("Options/Label").text = tr("MENU_OPTIONS")
+	get_node("Credits/Label").text = tr("MENU_CREDITS")
+	get_node("Exit/Label").text = tr("MENU_EXIT")
 
 func _on_Exit_pressed():
 	get_tree().quit()
