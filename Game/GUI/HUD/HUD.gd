ad87dabfffc73cc4646eed5ff62875b571131c82
Matías Muñoz Espinoza <writkas2@gmail.com>
2017-10-28 02:31:09 -0300

    Algunas animaciones del hud

--- Game/GUI/HUD/HUD.gd
+++ Game/GUI/HUD/HUD.gd
@@ -2,17 +2,24 @@ extends CanvasLayer
 
 onready var animation_inventory = $AnimationInventory
 onready var animation_stats = $AnimationStats
+onready var animation_bars = $AnimationBars
+onready var animation_hud_menu = $"AnimationHUD-Menu"
+onready var animation_photos_mini = $AnimationPhotosMini
+onready var animation_hotbar = $AnimationHotbar
 
-#onready var inventory = $Inventory
-#onready var stats = $Stats
-
+var bars_show = false
+var photos_mini_show = false
+var hotbar_show = false
+var hud_menu_show = false
 var inventory_show = false
 var stats_show = false
+
+enum HudElement {BARS, PHOTOS_MINI, HOTBAR, HUD_MENU}
 
 func _ready():
 	set_process_input(true)
 	
-func _input(event):	
+func _input(event):
 	if event.is_action_pressed("inventory") and not animation_inventory.is_playing():
 		if not inventory_show:
 			inventory_show = true
@@ -28,3 +35,57 @@ func _input(event):
 			stats_show = false
 			animation_stats.play("stats_hide")
 	
+	if GameGlobals.debug: debug_hud(event)
+
+func debug_hud(event):
+	if event.is_action_pressed("1"):
+		hud_show_and_hide(HudElement.BARS)
+	if event.is_action_pressed("2"):
+		hud_show_and_hide(HudElement.PHOTOS_MINI)
+	if event.is_action_pressed("3"):
+		hud_show_and_hide(HudElement.HOTBAR)
+	if event.is_action_pressed("4"):
+		hud_show_and_hide(HudElement.HUD_MENU)
+
+func hud_show_and_hide(_HudElement):
+	match _HudElement:
+		HudElement.BARS: bars_show_and_hide()
+		HudElement.PHOTOS_MINI: photos_mini_show_and_hide()
+		HudElement.HOTBAR: hotbar_show_and_hide()
+		HudElement.HUD_MENU: hud_menu_show_and_hide()
+			
+func bars_show_and_hide():
+	if not animation_bars.is_playing():
+		if not bars_show:
+			animation_bars.play("bars_show")
+			bars_show = true
+		else:
+			animation_bars.play("bars_hide")
+			bars_show = false
+
+func photos_mini_show_and_hide():
+	if not animation_photos_mini.is_playing():
+		if not photos_mini_show:
+			animation_photos_mini.play("photos_mini_show")
+			photos_mini_show = true
+		else:
+			animation_photos_mini.play("photos_mini_hide")
+			photos_mini_show = false
+
+func hotbar_show_and_hide():
+	if not animation_hotbar.is_playing():
+		if not hotbar_show:
+			animation_hotbar.play("hotbar_show")
+			hotbar_show = true
+		else:
+			animation_hotbar.play("hotbar_hide")
+			hotbar_show = false
+			
+func hud_menu_show_and_hide():
+	if not animation_hud_menu.is_playing():
+		if not hud_menu_show:
+			animation_hud_menu.play("hud_menu_show")
+			hud_menu_show = true
+		else:
+			animation_hud_menu.play("hud_menu_hide")
+			hud_menu_show = false
